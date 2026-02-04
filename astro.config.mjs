57b69b639952935183c9b5b1d9cf6c1b666b2737
baptistesgd <baptiste.sgd@gmail.com>
2026-02-04 22:03:20 +0100

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://pole-energie-autonomie.fr',
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+  output: 'static',
 });
