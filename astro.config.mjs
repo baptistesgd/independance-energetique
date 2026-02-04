5b3aac1d9aedacf1603116b0f60048d83cb66f50
baptistesgd <baptiste.sgd@gmail.com>
2026-02-04 21:27:45 +0100

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://pole-energie-autonomie.fr',
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
+  },
+  vite: {
+    ssr: {
+      noExternal: ['react-icons'],
+    },
+  },
+});
